6c1374cb8959ce6832e13e538337dd311e1f68fd
Noah Diderich <noah.diderich@student.kdg.be>
2023-12-05 23:41:53 +0100

    concatenating all the new data into one df

--- function_app.py
+++ function_app.py
@@ -8,16 +8,16 @@ import pandas as pd
 
 app = func.FunctionApp()
 
+# function triggers on a timer every 15 minutes
+@app.function_name(name="sendDataToBA")
 @app.schedule(schedule="* */15 * * *", arg_name="myTimer", run_on_startup=True,
               use_monitor=False) 
 def sendDataToBA(myTimer: func.TimerRequest) -> None:
-    send_data_to_queue()
-    if myTimer.past_due:
-        logging.info('The timer is past due!')
-
     logging.info('Python timer trigger function executed.')
+    send_data_to_queue()
 
 def send_data_to_queue():
+    # connect to rabbitmq using the right credentials
     connection_params = pika.ConnectionParameters(
         host='localhost',
         port=5673,
@@ -28,26 +28,72 @@ def send_data_to_queue():
     connection = pika.BlockingConnection(connection_params)
     channel = connection.channel()
 
+    # declare the queue
     queue_name = 'new_data_queue'
     channel.queue_declare(queue=queue_name, durable=True)
     
+    # connect to blob storage using the connection string
     connection_string = os.environ["MyStorageAccountConnection"]
     blob_service_client = BlobServiceClient.from_connection_string(connection_string)
+
+    # get all the blobs names in the csv container
     container_client = blob_service_client.get_container_client("csv")
     blobs = container_client.list_blobs()
+    
+    # get the grid from the blob storage
+    grid = download_blob_to_file(blob_service_client, "grid", "grid.csv")
+
+    # for every blob, download it to a dataframe and add it to a list
+    new_data = []
     for b in blobs:
-        print(b.name)
-        download_blob_to_file(blob_service_client, "csv", b.name)
+        df = download_blob_to_file(blob_service_client, "csv", b.name)
+        new_data.append(df)
+        logging.info(f"downloaded {b.name}")
+    
+    # concatenate all the dataframes into one big dataframe
+    concatenated_data = concatenate_new_data(grid, new_data)
 
-    channel.basic_publish(exchange='', routing_key=queue_name, body="hello")
-    logging.info(f"sent hello to queue {queue_name}")
+    # convert the dataframe to json and send it to the queue
+    json_string = concatenated_data.to_json(orient='records', lines=True)
+    channel.basic_publish(exchange='', routing_key=queue_name, body=json_string)
+    print(json_string)
+    logging.info(f"sent new data to queue {queue_name}")
 
+    # close the connection
     connection.close()
 
 def download_blob_to_file(blob_service_client: BlobServiceClient, container_name, blob_name):
     blob_client = blob_service_client.get_blob_client(container=container_name, blob=blob_name)
     blob_data = blob_client.download_blob()
+
     csv_file = StringIO(blob_data.readall().decode('utf-8'))
     df = pd.read_csv(csv_file)
     return df
- 
+
+def concatenate_new_data(grid, new_data):
+    # create a dataframe with the right columns
+    columns = ["square_uuid", "timestamp", "valueType", "value"]
+    concatenated_data = pd.DataFrame(columns=columns)
+
+    # loop through all the squares in the grid
+    for index, grid_row in grid.iterrows():
+        # loop through all the dataframes
+        for df in new_data:
+
+            # filter all the rows that contain data that are in the square
+            data_in_square = df[
+                (df["latitude"] < grid_row["latStart"]) & 
+                (df["latitude"] > grid_row["latEnd"]) & 
+                (df["longitude"] > grid_row["longStart"]) & 
+                (df["longitude"] < grid_row["longEnd"])
+                ]
+            
+            # add those rows to the new dataframe
+            for index, data_row in data_in_square.iterrows():
+                concatenated_data.loc[len(concatenated_data)] = {
+                    "square_uuid": grid_row["uuid"],
+                    "timestamp": data_row["timestamp"],
+                    "valueType": data_row["valueType"],
+                    "value": data_row["sensorDataValue"]
+                }
+    return concatenated_data
